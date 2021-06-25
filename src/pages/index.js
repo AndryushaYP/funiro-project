d2b400365c7bf9d7fba55797f8c412486743f22d
Andrey Kloninger <veto.aion@gmail.com>
2021-06-25 14:51:39 +0500

    [add]: products

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,47 @@
 import "../pages/index.scss";
+import { products, productsList, buttonMore } from "../utils/constants";
+
+const createProduct = () => {
+  const template = document.querySelector("#product-template").content;
+  const productElement = template.querySelector(".product__overlay").cloneNode(true);
+  const productButtons = productElement.querySelector(".products__buttons");
+  productElement.addEventListener("mouseover", (evt) => {
+    productButtons.style.display = "flex";
+  });
+  productElement.addEventListener("mouseout", (evt) => {
+    productButtons.style.display = "none";
+  });
+  return productElement;
+};
+const getProducts = (start, end) => {
+  products.slice(start, end).forEach((item) => {
+    const productElement = createProduct();
+    productElement.querySelector(".products__image").src = item.image;
+    productElement.querySelector(".products__title").textContent = item.title;
+    productElement.querySelector(".products__description").textContent = item.text;
+    productElement.querySelector(".products__price").textContent = `Rp ${item.price}`;
+    productElement.querySelector(".products__label").textContent = item.label;
+    if (item.label === "New") {
+      productElement.querySelector(".products__label").classList.add("products__label_new");
+    } else if (item.label === "") {
+      productElement.querySelector(".products__label").style.display = "none";
+    } else {
+      productElement.querySelector(".products__label").classList.add("products__label_sale");
+    }
+    if (item.priceOld !== "") {
+      productElement.querySelector(".products__price_old").textContent = `Rp ${item.priceOld}`;
+    }
+    productElement.querySelector(".products__buttons");
+    productsList.append(productElement);
+  });
+};
+
+buttonMore.addEventListener("click", () => {
+  const productsArr = productsList.querySelectorAll(".products__list-item");
+  if (productsArr.length !== products.length) {
+    getProducts(4, 8);
+    buttonMore.style.display = "none";
+  }
+});
+
+getProducts(0, 4);
---
+++ src/utils/constants.js
@@ -0,0 +1,103 @@
+import img1 from "../images/products/01.jpg";
+import img2 from "../images/products/02.jpg";
+import img3 from "../images/products/03.jpg";
+import img4 from "../images/products/04.jpg";
+import img5 from "../images/products/05.jpg";
+import img6 from "../images/products/06.jpg";
+import img7 from "../images/products/07.jpg";
+import img8 from "../images/products/08.jpg";
+export const productsList = document.querySelector(".products__list");
+export const buttonMore = document.querySelector(".products__button-more");
+export const productsArr = productsList.querySelectorAll(".products__list-item");
+export const productsButtons = document.querySelector(".psoducts__buttons");
+export const products = [
+  {
+    id: 1,
+    url: "#",
+    title: "Syltherine",
+    text: "Stylish cafe chair",
+    label: "-30%",
+    image: img1,
+    price: "2.500.000",
+    priceOld: "3.500.000",
+    isLiked: false,
+  },
+  {
+    id: 2,
+    url: "#",
+    title: "Leviosa",
+    text: "Stylish cafe chair",
+    label: "-30%",
+    image: img2,
+    price: "1.500.000",
+    priceOld: "",
+    isLiked: false,
+  },
+  {
+    id: 3,
+    url: "#",
+    title: "Lolito",
+    text: "Luxury big sofa",
+    label: "-50%",
+    image: img3,
+    price: "7.000.000",
+    priceOld: "14.000.000",
+    isLiked: false,
+  },
+
+  {
+    id: 4,
+    url: "#",
+    title: "Respira",
+    text: "Minimalist fan",
+    label: "New",
+    image: img4,
+    price: "500.000",
+    priceOld: "",
+    isLiked: false,
+  },
+  {
+    id: 5,
+    url: "#",
+    title: "Grifo",
+    text: "Night Lamp",
+    label: "",
+    image: img5,
+    price: "1.500.000",
+    priceOld: "",
+    isLiked: false,
+  },
+  {
+    id: 6,
+    url: "#",
+    title: "Muggo",
+    text: "Small mug",
+    label: "New",
+    image: img6,
+    price: "150.000",
+    priceOld: "",
+    isLiked: false,
+  },
+  {
+    id: 7,
+    url: "#",
+    title: "Pingky",
+    text: "Cute bed set",
+    label: "-50%",
+    image: img7,
+    price: "7.000.000",
+    priceOld: "14.000.000",
+    isLiked: false,
+  },
+  {
+    id: 8,
+    url: "#",
+    title: "Potty",
+    text: "minimalist flower pot",
+    label: "New",
+    image: img8,
+    price: "500.000",
+    priceOld: "",
+    isLiked: false,
+  },
+];
